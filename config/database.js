45173d8cb2d9bee2b38326e57274d0f8297d280b
Dheeraj.kumar <dheerajkrao1993@gmail.com>
2017-07-29 10:15:08 +0530

    all done

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	remoteurl : 'mongodb://<dbuser>:<dbpassword>@ds115493.mlab.com:15493/nodetodo',
+	localurl:'mongodb://localhost:27017/nodeTodo'
+};
---
+++ public/js/core.js
@@ -0,0 +1,1 @@
+angular.module('nodetodo', ['todoController', 'todoService']);
